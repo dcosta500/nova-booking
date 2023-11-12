bc867607fe57e30a8afaaef4f1ea1f8375c8a0ca
Diogo Costa <diogo.alex.nc@gmail.com>
2023-11-12 21:30:01 +0000

    Default profile added.

--- src/app/NavBar/Navbar.js
+++ src/app/NavBar/Navbar.js
@@ -5,14 +5,17 @@ import Typography from "@mui/material/Typography";
 // css
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   // Fetch object from store
+  const user = useSelector((store) => store.user);
   const navigate = useNavigate();
 
   const images = {
     logo: require("src/images/typo_logo.png"),
     avatar: require("src/images/profile_pic.jpg"),
+    defaultAvatar: require("src/images/profile-default.jpg"),
   };
 
   const teal = "#456268";
@@ -55,7 +58,7 @@ const Navbar = () => {
           <img
             onClick={() => navigate("/nova-booking/profile")}
             className="avatar"
-            src={images.avatar}
+            src={user.name == undefined ? images.defaultAvatar : images.avatar}
             alt="Avatar"
           />
         </Box>
